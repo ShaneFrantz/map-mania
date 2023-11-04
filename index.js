f8b60d234275fbb0af88ac8a51acd3baad3617e3
Shane Frantz <shanesfrantz@gmail.com>
2023-11-04 16:01:29 -0500

    Requirement 5 added

--- index.js
+++ index.js
@@ -1,53 +1,91 @@
-// Notes relating to differences from other tutorials:
-// 1 - Renamed "Map, Map, Map" to "myMapID, gMap, map"
-
 var gMap;
+var markers = [];
+var gameWon = false;
+var currentLocationIndex = 0;
 
-// initMap is a callback function that is initiated as part of the Google Maps API call at the bottom
-// of the HTML file
+var conventionLocations = [
+    { name: '2013 Convention', lat: 43.6532, lng: -79.3832 },
+    { name: '2014 Convention', lat: 36.1716, lng: -115.1391 },
+    { name: '2015 Convention', lat: 40.4406, lng: -79.9959 },
+    { name: '2016 Convention', lat: 36.1627, lng: -86.7816 },
+    { name: '2018 Convention', lat: 28.5384, lng: -81.3789 },
+    { name: '2019 Convention', lat: 40.7608, lng: -111.8910 },
+    { name: '2022 Convention', lat: 35.2271, lng: -80.8431 },
+    { name: '2023 Convention', lat: 38.2527, lng: -85.7585 }
+];
+
+var playerScore = 0;
+
 function initMap() {
-    // Create a new map and assign it to gMap
     gMap = new google.maps.Map(document.getElementById('myMapID'), {
-        center: {lat: 41.878, lng: 10}, zoom: 3})
+        center: { lat: 37.0902, lng: -95.7129 },
+        zoom: 4
+    });
 
-    // Add marker for Spectrum Center
-    var marker = new google.maps.Marker({position:{lat:35.2252,lng:-80.8393}, map:gMap})
-    // Add marker for KFC Yum Center
-    var marker2 = new google.maps.Marker({position:{lat:38.2578,lng:-85.7539}, map:gMap})
+    document.getElementById('topText').textContent = 'Start looking for ' + conventionLocations[0].name + '!';
 
-    console.log(location.lat)
-    console.log(location.lng)
+    conventionLocations.forEach(function (location, index) {
+        var marker = new google.maps.Marker({ position: { lat: location.lat, lng: location.lng }, map: gMap, visible: false });
+        markers.push(marker);
+    });
 
-    var infoWindow = new google.maps.InfoWindow({content:'2022 BHS International Convention'})
-    marker2.addListener('click', function() {
-        infoWindow.open(gMap, marker)
-    })
+    google.maps.event.addListener(gMap, 'idle', function () {
+        var zoomLevel = gMap.getZoom();
 
-    var infoWindow2 = new google.maps.InfoWindow({content:'2023 BHS International Convention'})
-    marker2.addListener('click', function() {
-        infoWindow2.open(gMap, marker2)
-    })
-
-    // Note that several message boards suggested using 'idle' instead of 'bounds_changed' because
-    // 'bounds_chaged' gets called over and over when the user drags the map
-    google.maps.event.addListener(gMap, 'idle', function() {
-        updateGame()
-    })
-}
-
-function updateGame() {
-    console.log('function updateGame()!')
-    var zoomLevel = gMap.getZoom()
-    console.log(zoomLevel)
-    var inBounds = false
-
-    // Check if location is on the map
-    if (gMap.getBounds().contains({lat:38.2578,lng:-85.7539})) {
-        inBounds = true
-    }
-    console.log(inBounds)
+        if (!gameWon && zoomLevel >= 8) {
+            if (currentLocationIndex < conventionLocations.length &&
+                gMap.getBounds().contains({ lat: conventionLocations[currentLocationIndex].lat, lng: conventionLocations[currentLocationIndex].lng })) {
+                var marker = markers[currentLocationIndex];
+                playerScore++;
+                console.log(playerScore);
+                marker.setVisible(true);
+                gMap.setCenter({ lat: 37.0902, lng: -95.7129 });
+                gMap.setZoom(4);
+                currentLocationIndex++;
+                if (playerScore === conventionLocations.length) {
+                    gameWon = true;
+                    alert('Congratulations! You found all convention locations. You win!');
+                    document.getElementById('topText').textContent = 'Congraulations! All Locations Found!';
+                    document.getElementById('scoreText').style.display = 'none';
+                } else {
+                    document.getElementById('topText').textContent = 'Location found! Now look for ' + conventionLocations[playerScore].name + '!';
+                    document.getElementById('scoreText').textContent = 'Score: ' + playerScore;
+                }
+            }
+        }
+    });
 }
 
 function initApplication() {
-    console.log('Map Mania Lite - Starting!')
+    console.log('Map Mania - Starting!')
+}
+
+function showHint() {
+    if (currentLocationIndex < conventionLocations.length) {
+        var currentHint = getHint(conventionLocations[currentLocationIndex].name);
+        alert(currentHint);
+    }
+}
+
+function getHint(locationName) {
+    switch (locationName) {
+        case '2013 Convention':
+            return 'Hint: Masterpiece';
+        case '2014 Convention':
+            return 'Hint: Musical Island Boys';
+        case '2015 Convention':
+            return 'Hint: Instant Classic';
+        case '2016 Convention':
+            return 'Hint: Forefront';
+        case '2018 Convention':
+            return 'Hint: After Hours';
+        case '2019 Convention':
+            return 'Hint: Signature';
+        case '2022 Convention':
+            return 'Hint: Quorum';
+        case '2023 Convention':
+            return 'Hint: Midtown';
+        default:
+            return 'No hint available';
+    }
 }
